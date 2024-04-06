f03315db929f0d829a390cd3ddfd5dcaec664cdf
Breno Saraiva <133717342+BrenoSaraiva-exe@users.noreply.github.com>
2024-04-06 10:38:58 -0300

    Update buscaEstoque.c

--- ATIVIDADE-N1/buscaEstoque.c
+++ ATIVIDADE-N1/buscaEstoque.c
@@ -1,3 +1,14 @@
+/* 
+* ---------------------------------------------------------------------- *
+* FATEC-São Caetano do Sul Estrutura de Dados                            *
+*                                                Avaliação N1 - Parte I  *
+* Objetivo: Controlar Estoque de produtos                                *
+*                                                                        *
+* Autores: <nomes que compõem a dupla>                                   *
+*                                                     Data:02/04/2024    *
+* ---------------------------------------------------------------------- *
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
